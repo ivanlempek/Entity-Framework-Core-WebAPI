f71d10db90a95638de2ebe820603689db307c2d4
ivanlmpk <ivanlempek@hotmail.com>
2022-08-10 00:27:36 -0300

    Separando a API em Camadas e inserindo o CRUD Básico(2).

---
+++ EFCore.Repository/Migrations/20220801052024_Init.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Battles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DtBegining = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DtEnd = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Battles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Heros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guns", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Guns_Heros_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HerosBattles",
+                columns: table => new
+                {
+                    HeroId = table.Column<int>(type: "int", nullable: false),
+                    BattleId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HerosBattles", x => new { x.BattleId, x.HeroId });
+                    table.ForeignKey(
+                        name: "FK_HerosBattles_Battles_BattleId",
+                        column: x => x.BattleId,
+                        principalTable: "Battles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HerosBattles_Heros_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SecretIdentities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RealName = table.Column<int>(type: "int", nullable: false),
+                    HeroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SecretIdentities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SecretIdentities_Heros_HeroId",
+                        column: x => x.HeroId,
+                        principalTable: "Heros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guns_HeroId",
+                table: "Guns",
+                column: "HeroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HerosBattles_HeroId",
+                table: "HerosBattles",
+                column: "HeroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SecretIdentities_HeroId",
+                table: "SecretIdentities",
+                column: "HeroId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Guns");
+
+            migrationBuilder.DropTable(
+                name: "HerosBattles");
+
+            migrationBuilder.DropTable(
+                name: "SecretIdentities");
+
+            migrationBuilder.DropTable(
+                name: "Battles");
+
+            migrationBuilder.DropTable(
+                name: "Heros");
+        }
+    }
+}
---
+++ EFCore.WebAPI_net6/Controllers/ValuesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using EFCore.Domain;
+using EFCore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace EFCore.WebAPI_net6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // Vamos fazer o get dos nossos elementos agora usando o Linq
+        [HttpGet]
+        public ActionResult Get()
+        {
+            // Vamos instanciar e chamar a nossa conexão com o banco de dados com o new HeroContext
+            using (var context = new HeroContext())
+            {
+                // Aqui usamos o método 'ToList' do Linq para listar os herois
+                var listHeros = context.Heros.ToList();
+                return Ok(listHeros);
+            }
+        }
+
+        // Get por Nome usando o Linq por querie ou lambda expression
+        [HttpGet("filter/{name}")]
+        public ActionResult GetFilter(string name)
+        {
+            using (var context = new HeroContext())
+            {
+                // Fazendo o get com as queryes do linq
+                /*var listHeroName = (from hero in context.Heros
+                    where hero.Name.Contains(name)
+                    select hero).ToList();*/
+
+                // Fazendo o get com lambda expression
+                var listHeroName = context.Heros.Where(h => h.Name.Contains(name)).ToList();
+                return Ok(listHeroName);
+
+                // Um detalhe é que sempre que terminamos de usar o ToList a conexão com o banco é fechada
+                // Para executar várias funções sem interromper a conexão é bom usar um foreach
+                /*foreach (var item in listHeroName)
+                {
+                    realizaCalculo();
+                    criaArquivos();
+                    salvaRelatorio();
+                }*/
+            }
+        }
+
+        // EndPoint para inserir um herói
+        [HttpPost("{nameHero}")]
+        public void Post(string nameHero)
+        {
+            // Aqui é o objeto 'Hero' que vamos criar para fazer o insert
+            // Dentro do objeto nós vamos colocar os parâmetros para a criação do 'Heroi'
+            // Quando vc coloca nos parâmetros um name sem um id
+            // O entityframework entende que isso é um insert
+            // Caso estivesse com um id junto ele iria entender que seria um update ( mesmo tendo um Heros.Add ali embaixo)
+            var hero = new Hero {Name = nameHero};
+            // o using vai realizar o insert
+            using (var context = new HeroContext())
+            {
+                // Agora a var hero se torna o objeto heroi do banco, recebendo aqui todos seus atributos
+                // para esse endpoint funcionar basta dar /update/'nomeDoHeroi'
+                hero.Name = nameHero;
+
+                // Aqui vamos utilizar o contexto para fazer o insert
+                // .Heros.Add estamos explicitando que vamos inserir um heroi
+                context.Heros.Add(hero);
+                context.SaveChanges();
+            }
+        }
+
+        // EndPoint para adicionar novos Heróis
+        [HttpGet("AddRange")]
+        public void GetAddRange()
+        {
+            using (var context = new HeroContext())
+            {
+                // Aqui vamos adicionar vários herois via AddRange
+                // Usando o addrange podemos adicionar vários objetos do tipo 'hero'
+                // E como o tipo é 'Hero' todos esses novos objetos vão ser automaticamente adicionados na tabela Hero
+                context.AddRange(
+                    new Hero {Name = "Capitão América"},
+                    new Hero {Name = "Doutor Estranho"},
+                    new Hero {Name = "Homem Aranha"},
+                    new Hero {Name = "Hulk"},
+                    new Hero {Name = "Capitão Marvel"},
+                    new Hero {Name = "Cable"}
+                );
+                context.SaveChanges();
+            }
+        }
+
+        // EndPoint de atualização de um herói
+        [HttpPut("{id}")]
+        public void Put(int id, string name)
+        {
+            {
+                using (var context = new HeroContext())
+                {
+                    // Agora estamos usando esse endpoint para o método UPDATE
+                    // Logo vamos dar o update por id
+                    // Esse id = 1 é o número da id do banco de dados que vai ser retornado para essa var hero
+                    // Aqui, através do contexto(banco), fazemos um SELECT dos Herois onde os Id's são iguais a 1
+                    // FirsOrDeafult retorna o primeiro ou o padrão
+                    var hero = context.Heros.Where(h => h.Id == id).Single();
+
+                    // Aqui vamos fazer o UPDATE do atributo nome do id escolhido
+                    hero.Name = name;
+
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        // EndPoint para deletar um herói
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            using (var context = new HeroContext())
+            {
+                // Vamos criar a variável hero e atribuir o nosso hero pelo id do contexto (banco de dados)
+                // E vamos usar uma lambda expression para passar o id como parãmetro
+                var hero = context.Heros.Where(x => x.Id == id).Single();
+
+                // E aqui usamos o Remove para remover o heroi que acabamos de chamar com a variável de cima
+                context.Heros.Remove(hero);
+                context.SaveChanges();
+            }
+        }
+    }
+}
